3e8f73e22e1a93e5d6346b689e9ce22e2a96f739
dnunez02 <dan@cs.tufts.edu>
2013-07-10 19:06:49 -0400

    Added comments to main.cpp for later reading

--- tutorial/main.cpp
+++ tutorial/main.cpp
@@ -1,19 +1,26 @@
 #include <SFML/Graphics.hpp>
 
 int main(){
+
+  //Create window of a w,h resolution with title
   sf::RenderWindow window(sf::VideoMode(200,200), "SFML Works");
+
+  //Create green circle
   sf::CircleShape shape(100.f);
   shape.setFillColor(sf::Color::Green);
   
+  //Main loop for window
   while(window.isOpen()){
     sf::Event event;
 
+    //Main event loop
     while(window.pollEvent(event)){
       if(event.type == sf::Event::Closed){
 	window.close();
       }
     }
 
+    //Render information [Essentially render loop]
     window.clear();
     window.draw(shape);
     window.display();
