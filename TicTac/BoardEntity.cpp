8cac7eb6b79bb60561f90ff85a09b0131d037988
dnunez02 <dan@cs.tufts.edu>
2013-07-25 12:36:25 -0400

    Updated BoardEntity to hold game logic, place items in specific places
    TicTacApp/BoardEntity.h
      - Added constants related to game logic
      - Added place() methods for CircleEntity and CrossEntity
      - Added int[] to represent on screen display of entities
    
    TicTacApp/BoardEntity.cpp
      - Defined both place() methods [uses geometric intersection]
      - Defined winner() to determine when game is over
      - Changed board to use 9 rectangle shapes, one for each empty space
        instead of 4 for lines. Better looking board.

--- TicTac/BoardEntity.cpp
+++ TicTac/BoardEntity.cpp
@@ -1,23 +1,122 @@
 #include "BoardEntity.h"
 
 BoardEntity::BoardEntity(float width, float height){
+  for(int i = 0; i < NUM_SPACES; ++i){
+    board[i] = EMPTY;
+  }
+
   pieces = new Array<sf::RectangleShape>();
 
-  sf::RectangleShape *piece = new sf::RectangleShape(sf::Vector2f(20, height));
-  piece->setPosition(sf::Vector2f(width/3.0 - 10, 0));
-  pieces->add(piece);
+  //create the visible board
+  sf::RectangleShape *piece;
+  for(int c = 0; c < NUM_COLS; ++c){
+    for(int r = 0; r < NUM_ROWS; ++r){
+      piece = new sf::RectangleShape(sf::Vector2f(width/NUM_COLS, height/NUM_ROWS));  
+      piece->setFillColor(sf::Color::Transparent);
+      piece->setOutlineThickness(3);
+      piece->setPosition(r * (width/NUM_COLS), c * (height/NUM_ROWS));
+      pieces->add(piece);
+    }
+  }
 
-  piece = new sf::RectangleShape(sf::Vector2f(20, height));
-  piece->setPosition(sf::Vector2f((width/3.0) * 2 - 10, 0));
-  pieces->add(piece);
+}
 
-  piece = new sf::RectangleShape(sf::Vector2f(width, 20));
-  piece->setPosition(sf::Vector2f(0, height/3.0 - 10));
-  pieces->add(piece);
+bool BoardEntity::place(CircleEntity *circ, int x, int y){
+  sf::RectangleShape *piece;
+  sf::Vector2f size, position;
+  float thickness = circ->getOutlineThickness();
 
-  piece = new sf::RectangleShape(sf::Vector2f(width, 20));
-  piece->setPosition(sf::Vector2f(0, (height/3.0) * 2 - 10));
-  pieces->add(piece);
+  for(int i = 0; i < pieces->size(); ++i){
+    piece = pieces->get(i);
+    position = piece->getPosition();
+    size = piece->getSize();
+
+    //If the circle intersects with one of the pieces
+    if(x > position.x && x < (position.x + size.x)) {
+      if(y > position.y && y < (position.y + size.y)) {
+
+	//And there is nothing in that spot, place it
+	if(board[i] == EMPTY){
+	  board[i] = CIRCLE;
+
+	  if(size.x < size.y)
+	    circ->setRadius(size.x / 2 - thickness);
+	  else
+	    circ->setRadius(size.y / 2 - thickness);
+
+	  circ->setPosition(position.x + thickness, 
+			    position.y + thickness);
+	  return true;
+	}
+      }
+    }
+  }
+
+  return false;
+}
+
+bool BoardEntity::place(CrossEntity *cross, int x, int y){
+  sf::RectangleShape *piece;
+  sf::Vector2f size, position;
+
+  for(int i = 0; i < pieces->size(); ++i){
+    piece = pieces->get(i);
+    position = piece->getPosition();
+    size = piece->getSize();
+
+    //If the circle intersects with one of the pieces
+    if(x > position.x && x < (position.x + size.x)) {
+      if(y > position.y && y < (position.y + size.y)) {
+
+	//And there is nothing in that spot, place it
+	if(board[i] == EMPTY){
+	  board[i] = CROSS;
+
+	  cross->setPosition(position.x, position.y);
+	  return true;
+	}
+      }
+    }
+  }
+
+  return false;
+}
+
+int BoardEntity::winner(){
+  int winner = NO_WIN;
+
+  //check rows for win
+  for(int i = 0; i < NUM_SPACES; i+=3){
+    winner = board[i] + board[i + 1] + board[i + 2];
+    if(winner == PLAYER_ONE_WIN)
+      return PLAYER_ONE;
+    if(winner == PLAYER_TWO_WIN)
+      return PLAYER_TWO;
+  }
+
+  //check cols for win
+  for(int i = 0; i < 3; ++i){
+    winner = board[i] + board[i + 3] + board[i + 6];
+    if(winner == PLAYER_ONE_WIN)
+      return PLAYER_ONE;
+    if(winner == PLAYER_TWO_WIN)
+      return PLAYER_TWO;
+  }
+
+  //check diags for win
+  winner = board[0] + board[4] + board[8];
+  if(winner == PLAYER_ONE_WIN)
+      return PLAYER_ONE;
+    if(winner == PLAYER_TWO_WIN)
+      return PLAYER_TWO;
+
+  winner = board[2] + board[4] + board[6];
+    if(winner == PLAYER_ONE_WIN)
+      return PLAYER_ONE;
+    if(winner == PLAYER_TWO_WIN)
+      return PLAYER_TWO;
+
+    return NO_WIN;
 }
 
 void BoardEntity::draw(sf::RenderTarget &target, sf::RenderStates states) const {
--- TicTac/BoardEntity.h
+++ TicTac/BoardEntity.h
@@ -2,6 +2,8 @@
 #define __BOARD_ENTITY_H__
 
 #include "../include/BasicEntity.h"
+#include "CircleEntity.h"
+#include "CrossEntity.h"
 #include "../src/Array.cpp"
 
 using namespace DAN;
@@ -10,9 +12,29 @@ class BoardEntity : public BasicEntity {
  public:
   BoardEntity(float, float);
   ~BoardEntity();
+  bool place(CircleEntity *circ, int x, int y);
+  bool place(CrossEntity *circ, int x, int y);
+  int winner();
+
+  static const int NUM_SPACES = 9;
+  static const int NUM_ROWS = 3;
+  static const int NUM_COLS = 3;
+
+  static const int DRAW = -2;
  private:
   Array<sf::RectangleShape> *pieces;
   virtual void draw(sf::RenderTarget &target, sf::RenderStates states) const;
+  int board[NUM_SPACES];
+
+  static const int EMPTY = 0;
+  static const int CIRCLE = 1;
+  static const int CROSS = -1;
+  static const int PLAYER_ONE_WIN = 3;
+  static const int PLAYER_TWO_WIN = -3;
+  static const int NO_WIN = -1;
+
+  static const int PLAYER_ONE = 0;
+  static const int PLAYER_TWO = 1;
 };
 
 #endif
